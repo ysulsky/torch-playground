800e8fe62fa575cb6cd365434055333d1b4cfc88
Yury Sulsky <yury.sulsky@gmail.com>
2023-12-04 23:30:47 +0000

    Add kv caching and sampling.

--- src/model.py
+++ src/model.py
@@ -1,7 +1,9 @@
+import dataclasses
 import torch
+import torch.distributions as distributions
 import torch.nn as nn
-import torch.nn.functional as F
 
+Categorical = distributions.Categorical
 Tensor = torch.Tensor
 
 
@@ -22,31 +24,68 @@ class MultiHeadLinear(nn.Module):
     return torch.stack(outs, dim=self.axis)
 
 
-class AttentionBlock(nn.Module):
-  """Attention Block."""
+@dataclasses.dataclass
+class KVCache:
+  seq_len: int
+  size: int
+  k_cache: torch.Tensor
+  v_cache: torch.Tensor
 
   @classmethod
-  def causal_self_attention_mask(cls, seq_len: int) -> Tensor:
-    return torch.tril(torch.ones((seq_len, seq_len), dtype=bool))
+  def empty(
+      cls, batch_size: int, seq_len: int,
+      n_heads: int, d_model: int) -> 'KVCache':
+    d_head = d_model // n_heads
+    k_cache = v_cache = torch.zeros((batch_size, n_heads, seq_len, d_head))
+    return cls(seq_len=seq_len, size=0, k_cache=k_cache, v_cache=v_cache)
+
+  def to_drop_for_update(self, new_els: int) -> tuple[int, int]:
+    return max(0, self.size + new_els - self.seq_len)
+
+  def update(self, new_k: Tensor, new_v: Tensor) -> 'KVCache':
+    batch_size, n_heads, new_els, d_head = new_k.shape
+    to_drop = self.to_drop_for_update(new_els)
+    remaining = self.size - to_drop
+    pad_amt = self.seq_len - (remaining + new_els)
+    padding = torch.zeros((batch_size, n_heads, pad_amt, d_head))
+    return KVCache(
+        seq_len=self.seq_len,
+        size=remaining + new_els,
+        k_cache=torch.cat(
+            (self.k_cache[:, :, to_drop : to_drop + remaining, :],
+             new_k, padding), dim=-2),
+        v_cache=torch.cat(
+            (self.v_cache[:, :, to_drop : to_drop + remaining, :],
+             new_v, padding), dim=-2),
+    )
+
+
+class AttentionBlock(nn.Module):
+  """Attention Block."""
 
   def __init__(self, n_heads: int, d_model: int, dropout: float):
     super().__init__()
     if d_model % n_heads:
       raise ValueError(f"{d_model=} not divisibile by {n_heads=}")
     d_head = d_model // n_heads
-    self.attn_prenorm = nn.LayerNorm(d_model)
-    self.ffw_prenorm = nn.LayerNorm(d_model)
+    self.attn_prenorm = lambda x:x #nn.LayerNorm(d_model)
+    self.ffw_prenorm = lambda x:x #nn.LayerNorm(d_model)
     self.kfn = MultiHeadLinear(n_heads, d_model, d_head, axis=-3)
     self.qfn = MultiHeadLinear(n_heads, d_model, d_head, axis=-3)
     self.vfn = MultiHeadLinear(n_heads, d_model, d_head, axis=-3)
     self.ffw = nn.Sequential(nn.Linear(d_model, d_model * 4),
                              nn.GELU(),
                              nn.Linear(d_model * 4, d_model))
-    self.dropout = nn.Dropout(dropout)
+    self.dropout = lambda x: x#nn.Dropout(dropout)
+
+  @classmethod
+  def causal_self_attention_mask(cls, seq_len: int) -> Tensor:
+    return torch.tril(torch.ones((seq_len, seq_len), dtype=bool))
 
   def forward(
       self, x: Tensor, y: Tensor | None = None,
-      mask: Tensor | None = None) -> Tensor:
+      mask: Tensor | None = None,
+      cache: KVCache | None = None) -> tuple[Tensor, KVCache | None]:
     B, Tx, D = x.shape
     x_norm = self.attn_prenorm(x)
     if y is None:
@@ -54,55 +93,93 @@ class AttentionBlock(nn.Module):
       y_norm = x_norm
     else:
       y_norm = self.attn_prenorm(y)
-    _, Ty, _ = y.shape
+    Ty = y.shape[1] if cache is None else cache.seq_len
     assert B == y.shape[0] and D == y.shape[-1]
-    q = self.qfn(x_norm)  # [B, H, Tx, D//H]
+    q = self.qfn(x_norm)  # [B, H, Tx, D//H]466,  0.2759, -0.6625],
     k = self.kfn(y_norm)  # [B, H, Ty, D//H]
     v = self.vfn(y_norm)  # [B, H, Ty, D//H]
+    if cache is not None:
+      cache = cache.update(k, v)
+      k, v = cache.k_cache, cache.v_cache
     logits = torch.einsum('...xd,...yd->...xy', q, k)  # [B, H, Tx, Ty]
     logits = logits * v.shape[-1] ** -0.5
     logits = self.dropout(logits)
     if mask is not None:
       assert mask.shape == (Tx, Ty)
       logits = torch.where(mask, logits, torch.tensor(float('-inf')))
+
     attn_out = torch.softmax(logits, dim=-1) @ v  # [B, H, Tx, D//H]
     attn_out = torch.transpose(attn_out, -3, -2)  # [B, Tx, H, D//H]
     attn_out = torch.reshape(attn_out, (B, Tx, D))
     attn_out = self.dropout(attn_out)
     ffw_input = self.ffw_prenorm(x + attn_out)
-    return x + self.dropout(self.ffw(ffw_input))
+    return x + self.dropout(self.ffw(ffw_input)), cache
 
 
 class Transformer(nn.Module):
   """Decoder-only transformer."""
 
   def __init__(self, n_blocks: int, n_heads: int, seq_len: int, d_model: int,
-               dropout: float):
+               vocab_size: int, dropout: float):
     super().__init__()
     self.seq_len = seq_len
     self.d_model = d_model
-    self.mask = self.register_buffer(
-        'mask', AttentionBlock.causal_self_attention_mask(seq_len))
+    self.n_heads = n_heads
+    self.vocab_size = vocab_size
+    self.register_buffer(
+        'ffw_mask', AttentionBlock.causal_self_attention_mask(seq_len))
+    self.token_embeddings = nn.Embedding(vocab_size, d_model)
     self.position_embeddings = nn.Embedding(seq_len, d_model)
     self.blocks = nn.ModuleList([
         AttentionBlock(n_heads=n_heads, d_model=d_model, dropout=dropout)
         for _ in range(n_blocks)
     ])
+    self.proj = nn.Linear(d_model, vocab_size)
     self.dropout = nn.Dropout(p=dropout)
 
-  def forward(self, sequence: Tensor) -> Tensor:
-    B, T, D = sequence.shape
-    assert self.seq_len == T
-    assert self.d_model == D
-    x = sequence + self.position_embeddings.weight
-    x = self.dropout(x)
-    for block in self.blocks:
-      x = block(x, mask=self.mask)
-    return x
+  def initialize_cache(self, batch_size: int) -> list[KVCache]:
+    return [KVCache.empty(batch_size=batch_size,
+                          seq_len=self.seq_len,
+                          n_heads=self.n_heads,
+                          d_model=self.d_model)] * len(self.blocks)
 
-  def loss(self, sequence: Tensor, target: Tensor) -> float:
-    logits = self(sequence)
-    assert logits.shape[:-1] == target.shape
-    logits = torch.reshape(logits, (-1, self.d_model))
+  def forward(self, tokens: Tensor,
+              cache: list[KVCache] | None = None
+      ) -> tuple[Tensor, list[KVCache] | None]:
+    x = self.token_embeddings(tokens)
+    if cache is None:
+      x = x + self.position_embeddings.weight
+      mask = self.ffw_mask
+    else:
+      num_toks = tokens.shape[-1]
+      pos_start = cache[0].size - cache[0].to_drop_for_update(num_toks)
+      pos_end = pos_start + num_toks
+      x = x + self.position_embeddings.weight[pos_start : pos_end]
+      mask = self.ffw_mask[pos_start : pos_end]
+    x = self.dropout(x)
+    if cache is not None:
+      in_cache = cache
+    else:
+      in_cache = [None] * len(self.blocks)
+    out_cache = [None] * len(self.blocks)
+    for i, block in enumerate(self.blocks):
+      x, out_cache[i] = block(x, mask=mask, cache=in_cache[i])
+    if cache is not None:
+      cache = out_cache
+    return self.proj(x), cache
+
+  def loss(self, tokens: Tensor, target: Tensor) -> float:
+    logits, _ = self(tokens, None)
+    assert logits.shape == (*target.shape, self.vocab_size)
+    logits = torch.reshape(logits, (-1, self.vocab_size))
     target = torch.reshape(target, (-1,))
-    return F.cross_entropy(logits, target)
+    return Categorical(logits=logits).log_prob(target).mean()
+
+  def sample(self, prefix: Tensor) -> Tensor:
+    batch_size, prefix_len = prefix.shape
+    logits, cache = self(prefix, self.initialize_cache(batch_size))
+    samples = [Categorical(logits=logits[:, -1]).sample()]
+    while len(samples) + prefix_len < self.seq_len:
+      logits, cache = self(torch.unsqueeze(samples[-1], 1), cache=cache)
+      samples.append(Categorical(logits=logits[:, -1]).sample())
+    return torch.stack(samples, dim=1)
--- src/tests/model_test.py
+++ src/tests/model_test.py
@@ -1,10 +1,14 @@
+"""Tests for model code."""
+
 import unittest
 import torch
+import torch.testing
 from src import model
 
 
 class MultiHeadLinearTest(unittest.TestCase):
 
+  @torch.no_grad()
   def test_shape(self):
     batch_size = 2
     n_heads = 3
@@ -20,6 +24,7 @@ class MultiHeadLinearTest(unittest.TestCase):
 
 class AttentionBlockTest(unittest.TestCase):
 
+  @torch.no_grad()
   def test_self_attn_shape(self):
     batch_size = 2
     n_heads = 3
@@ -29,9 +34,10 @@ class AttentionBlockTest(unittest.TestCase):
                                dropout=0.5)
     mask = net.causal_self_attention_mask(seq_len)
     input = torch.zeros((batch_size, seq_len, d_model))
-    output = net(input, mask=mask)
+    output, _ = net(input, mask=mask)
     self.assertEqual(input.shape, output.shape)
 
+  @torch.no_grad()
   def test_cross_attn_shape(self):
     batch_size = 2
     n_heads = 3
@@ -42,27 +48,69 @@ class AttentionBlockTest(unittest.TestCase):
                                dropout=0.5)
     input = torch.zeros((batch_size, seq_len, d_model))
     context = torch.zeros((batch_size, enc_seq_len, d_model))
-    output = net(input, context, mask=None)
+    output, _ = net(input, context, mask=None)
     self.assertEqual(input.shape, output.shape)
 
 
 class TransformerTest(unittest.TestCase):
 
+  @torch.no_grad()
   def test_shape(self):
     batch_size = 2
     n_heads = 3
     d_model = 18
-    seq_len = 6
+    vocab_size = 24
+    seq_len = 2
 
-    seq = torch.randn((batch_size, seq_len, d_model))
+    seq = torch.randint(vocab_size, (batch_size, seq_len))
     net = model.Transformer(n_blocks=2, n_heads=n_heads, seq_len=seq_len,
-                            d_model=d_model, dropout=0.1)
-    out = net(seq)
-    self.assertEqual(seq.shape, out.shape)
-    target = torch.zeros((batch_size, seq_len), dtype=torch.long)
+                            d_model=d_model, vocab_size=vocab_size,
+                            dropout=0.1)
+    out, _ = net(seq)
+    self.assertEqual(out.shape, (batch_size, seq_len, vocab_size))
+    target = (vocab_size - 1) * torch.ones(
+      (batch_size, seq_len), dtype=torch.long)
     loss = net.loss(seq, target)
     self.assertEqual(loss.shape, ())
 
+  @torch.no_grad()
+  def test_cache_same_as_ffw(self):
+    n_blocks = 2
+    batch_size = 2
+    n_heads = 3
+    d_model = 18
+    vocab_size = 24
+    seq_len = 24
+
+    seq = torch.randint(vocab_size, (batch_size, seq_len))
+    net = model.Transformer(n_blocks=n_blocks, n_heads=n_heads, seq_len=seq_len,
+                            d_model=d_model, vocab_size=vocab_size,
+                            dropout=0.0)
+    out_ffw, _ = net(seq)
+    out_loop = [None] * seq_len
+    cache = net.initialize_cache(batch_size)
+
+    for t in range(seq_len):
+      out_loop[t], cache = net(seq[:, t:t+1], cache=cache)
+    torch.testing.assert_allclose(out_ffw, torch.cat(out_loop, dim=1),
+                                  atol=1e-2, rtol=1e-2)
+
+  @torch.no_grad()
+  def test_sample_shape(self):
+    n_blocks = 2
+    batch_size = 2
+    n_heads = 3
+    d_model = 18
+    vocab_size = 24
+    seq_len = 24
+    prefix_len = 10
+
+    net = model.Transformer(n_blocks=n_blocks, n_heads=n_heads, seq_len=seq_len,
+                            d_model=d_model, vocab_size=vocab_size,
+                            dropout=0.1)
+    prefix = torch.randint(vocab_size, (batch_size, prefix_len))
+    suffix = net.sample(prefix)
+    self.assertEqual(suffix.shape, (batch_size, seq_len - prefix_len))
 
 
 if __name__ == "__main__":
